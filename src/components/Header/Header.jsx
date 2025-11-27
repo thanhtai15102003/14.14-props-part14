a5e9579938eb714085b9a2847a39005e74a4fe97
thanhtai15102003 <thainguyenthanhtai2003@gmail.com>
2025-11-27 18:04:56 +0700

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import logo from "../../assets/logo-tuhoc.png";
+import "./Header.css";
+
+function Header() {
+  return (
+    <>
+      <header>
+        <img src={logo} alt="Tự Học" />
+        <h1>React Tuhoc.cc</h1>
+        <p>
+          Học React - Khám phá cách xây dựng ứng dụng linh hoạt, hiện đại, và
+          đầy sáng tạo!
+        </p>
+      </header>
+    </>
+  );
+}
+
+export default Header;
